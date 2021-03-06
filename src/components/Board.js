e46871fabffa5a56e38e4f6ff34879a54553a6ee
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-06 21:07:27 +0530

    added components

---
+++ src/components/Board.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Squ from './Squ';
+const Board = () => {
+    return (
+        <div>
+            <div>
+                <Squ/>
+                <Squ/>
+                <Squ/>
+            </div>
+            <div>
+                <Squ/>
+                <Squ/>
+                <Squ/>
+            </div>
+            <div>
+                <Squ/>
+                <Squ/>
+                <Squ/>
+            </div>
+        </div>
+    );
+}
+
+export default Board;
