fd1c2c3b151725ea9111aaf2e90c52722af6fe83
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-07 23:33:32 +0530

    calculated winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,9 @@
 import React,{useState} from 'react'
 import Square from './Square';
 
-const Board = () => {
+const Board = ({board,handleSquareClick}) => {
 
-    const [board,setBoard]=useState(Array(9).fill(null));
-    const [isXNext,setIsXNext]=useState(false);
-
-    const handleSquareClick= position =>{
-
-        if(board[position])
-        return;
-
-        setBoard(prev => {
-            return prev.map((square,pos)=>{
-                if(pos===position)
-                {
-                    return isXNext?'X':'0';
-                }
-                return square;
-            });
-        });
-        setIsXNext(prev=>!prev);
-    };
+   
 
     const renderSquare= position =>{
         return (
@@ -34,19 +16,19 @@ const Board = () => {
     return (
         <div className="board">
             <div className="board-row">
-                {renderSquare(0)};
-                {renderSquare(1)};
-                {renderSquare(2)};
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                {renderSquare(3)};
-                {renderSquare(4)};
-                {renderSquare(5)};
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                {renderSquare(6)};
-                {renderSquare(7)};
-                {renderSquare(8)};
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
