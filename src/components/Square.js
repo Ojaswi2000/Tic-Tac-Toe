698c6c8e70eea00ad90e6fa8ee1f063d805b6bd1
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-08 13:41:31 +0530

    styles entire app

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Square = ({value,onClick,isWinningSquare}) => {
     return <button type="button" 
-    className="square" 
     onClick={onClick}
+    className={`square ${isWinningSquare?'winning' :''} ${value==='X'?'text-green':'text-orange'}`}
     style={{
         fontWeight:isWinningSquare?'bold':'normal'
     }}
